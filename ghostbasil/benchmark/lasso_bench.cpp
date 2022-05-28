95a3cd55b37ba4411b6be98f8d13480fe6fe9186
James Yang <jamesyang916@gmail.com>
2022-05-28 16:46:09 -0400

    Add debug msg

--- ghostbasil/benchmark/lasso_bench.cpp
+++ ghostbasil/benchmark/lasso_bench.cpp
@@ -67,6 +67,9 @@ BENCHMARK_DEFINE_F(LassoFixture, lasso_bench)(benchmark::State& state)
     auto& A = std::get<0>(input);
     auto& r = std::get<1>(input);
 
+    PRINT(A.block(0,0,3,3));
+    PRINT(r.head(5));
+
     std::vector<double> lmdas(100);
     double factor = std::pow(1e-6, 1./(lmdas.size()-1));
     lmdas[0] = r.array().abs().maxCoeff();
