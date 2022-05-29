293235c856ccbaf9c33d7b8a9d18ab49cd19b31a
James Yang <jamesyang916@gmail.com>
2022-05-29 01:54:44 -0400

    Modify benchmark to see if sparse vector index type matters

--- ghostbasil/benchmark/sp_vec_vs_map_dot_bench.cpp
+++ ghostbasil/benchmark/sp_vec_vs_map_dot_bench.cpp
@@ -13,9 +13,12 @@ namespace {
 struct Fixture
     : benchmark::Fixture
 {
-    using vec_t = Eigen::Vector<double, Eigen::Dynamic>;
-    using sp_vec_t = Eigen::SparseVector<double>;
-    using map_sp_vec_t = Eigen::Map<sp_vec_t>;
+    using value_t = double;
+    using index_t = uint32_t;
+    using vec_t = Eigen::Vector<value_t, Eigen::Dynamic>;
+    using sp_vec_t = Eigen::SparseVector<value_t>;
+    using sp_vec_our_t = Eigen::SparseVector<value_t, Eigen::ColMajor, index_t>;
+    using map_sp_vec_our_t = Eigen::Map<const sp_vec_our_t>;
 
     auto make_input(
             size_t seed,
@@ -26,16 +29,18 @@ struct Fixture
         vec_t beta(p); 
         beta.setRandom();
 
-        sp_vec_t vs(p);
+        std::vector<index_t> innerIndex;
+        std::vector<value_t> values;
         std::normal_distribution<> norm(0., 1.);
         std::bernoulli_distribution bern(density);
-        for (size_t i = 0; i < vs.size(); ++i) {
-            if (bern(gen)) vs.coeffRef(i) = norm(gen);
+        for (size_t i = 0; i < p; ++i) {
+            if (bern(gen)) {
+                innerIndex.push_back(i);
+                values.push_back(norm(gen));
+            }
         }
 
-        map_sp_vec_t vs_map(vs.size(), vs.nonZeros(), vs.innerIndexPtr(), vs.valuePtr());
-
-        return std::make_tuple(beta, vs, vs_map);
+        return std::make_tuple(beta, innerIndex, values);
     }
 };
 
@@ -43,11 +48,15 @@ BENCHMARK_DEFINE_F(Fixture, sparse)(benchmark::State& state)
 {
     size_t seed = state.range(0);
     size_t p = state.range(1);
-    double density = 0.7;
+    double density = 0.3;
 
     auto input = make_input(seed, p, density);
     auto& beta = std::get<0>(input);
-    auto& vs = std::get<1>(input);
+    auto& innerIndex = std::get<1>(input);
+    auto& values = std::get<2>(input);
+
+    map_sp_vec_our_t vmap(beta.size(), innerIndex.size(), innerIndex.data(), values.data());
+    sp_vec_t vs = vmap;
 
     double res = 0;
 
@@ -69,17 +78,24 @@ BENCHMARK_DEFINE_F(Fixture, sparse_map)(benchmark::State& state)
 {
     size_t seed = state.range(0);
     size_t p = state.range(1);
-    double density = 0.7;
+    double density = 0.3;
 
     auto input = make_input(seed, p, density);
     auto& beta = std::get<0>(input);
-    auto& vs_map = std::get<2>(input);
+    auto& innerIndex = std::get<1>(input);
+    auto& values = std::get<2>(input);
+
+    map_sp_vec_our_t vs_our_map(
+            beta.size(), 
+            innerIndex.size(), 
+            innerIndex.data(),
+            values.data());
 
     double res = 0;
 
     for (auto _ : state) {
         benchmark::DoNotOptimize(
-                res = vs_map.dot(beta)
+                res = vs_our_map.dot(beta)
                 );
     }
 }
