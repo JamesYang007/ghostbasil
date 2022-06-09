1a5dc002d643574a2700cb32a76fcdd4b9cc0e4a
James Yang <jamesyang916@gmail.com>
2022-06-09 16:37:44 -0400

    Add benchmark for block dense

--- ghostbasil/benchmark/lasso_bench.cpp
+++ ghostbasil/benchmark/lasso_bench.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <iomanip>
 #include <ghostbasil/optimization/lasso.hpp>
+#include <ghostbasil/matrix/block_matrix.hpp>
+#include <tools/matrix/block_matrix.hpp>
 
 namespace ghostbasil {
 namespace {
@@ -10,7 +12,7 @@ namespace {
 struct LassoFixture
     : benchmark::Fixture
 {
-    static auto make_input(
+    static auto generate_data(
             size_t n, size_t p, size_t seed)
     {
         std::mt19937 gen(seed);
@@ -28,6 +30,57 @@ struct LassoFixture
         Eigen::MatrixXd A = X.transpose() * X / n;
         Eigen::VectorXd r = X.transpose() * y / n;
         return std::make_tuple(A, r);
+    }
+
+    template <class AType, class RType>
+    static auto make_input(const AType& A, const RType& r)
+    {
+        size_t p = A.cols();
+
+        std::vector<double> lmdas(100);
+        double factor = std::pow(1e-6, 1./(lmdas.size()-1));
+        lmdas[0] = r.array().abs().maxCoeff();
+        for (size_t i = 1; i < lmdas.size(); ++i) {
+            lmdas[i] = lmdas[i-1] * factor;
+        }
+
+        std::vector<uint32_t> strong_set(p);
+        std::iota(strong_set.begin(), strong_set.end(), 0);
+
+        auto strong_order = strong_set;
+
+        Eigen::VectorXd strong_A_diag(p);
+        for (size_t i = 0; i < strong_A_diag.size(); ++i) {
+            strong_A_diag[i] = A.coeff(i,i);
+        }
+
+        std::vector<double> strong_beta(strong_set.size(), 0);
+        std::vector<double> strong_grad(strong_set.size());
+        for (size_t i = 0; i < strong_grad.size(); ++i) {
+            strong_grad[i] = r[strong_set[i]];
+        }
+        std::vector<uint32_t> active_set;
+        std::vector<uint32_t> active_order;
+        std::vector<uint32_t> active_set_ordered;
+        std::vector<bool> is_active(strong_set.size(), false);
+
+        const auto orig_strong_grad = strong_grad;
+
+        std::vector<Eigen::SparseVector<double>> betas(lmdas.size());
+        std::vector<double> rsqs(lmdas.size());
+
+        size_t n_cds = 0;
+        size_t n_lmdas = 0;
+        double rsq = 0;
+        size_t max_cds = 100000;
+        double thr = 1e-7;
+
+        return std::make_tuple(
+                strong_set, strong_order, 
+                strong_A_diag, lmdas, max_cds, thr, rsq, strong_beta, 
+                strong_grad, active_set, active_order, active_set_ordered,
+                is_active, betas, rsqs, 
+                n_cds, n_lmdas);
     }
 
     template <class SGType, class StrongBetaType,
@@ -58,51 +111,117 @@ struct LassoFixture
     }
 };
 
-BENCHMARK_DEFINE_F(LassoFixture, lasso_bench)(benchmark::State& state) 
+BENCHMARK_DEFINE_F(LassoFixture, dense)(benchmark::State& state) 
 {
     size_t p = state.range(0);
     size_t n = 100;
     size_t seed = 30;
-    size_t max_cds = 100000;
-    double thr = 1e-7;
     double s = 0.5;
 
-    auto input = make_input(n, p, seed);
-    auto& A = std::get<0>(input);
-    auto& r = std::get<1>(input);
+    auto&& data = generate_data(n, p, seed);
+    auto&& A = std::get<0>(data);
+    auto&& r = std::get<1>(data);
 
-    std::vector<double> lmdas(100);
-    double factor = std::pow(1e-6, 1./(lmdas.size()-1));
-    lmdas[0] = r.array().abs().maxCoeff();
-    for (size_t i = 1; i < lmdas.size(); ++i) {
-        lmdas[i] = lmdas[i-1] * factor;
+    auto&& input = make_input(A, r);
+    auto&& strong_set = std::get<0>(input);
+    auto&& strong_order = std::get<1>(input);
+    auto&& strong_A_diag = std::get<2>(input);
+    auto&& lmdas = std::get<3>(input);
+    auto&& max_cds = std::get<4>(input);
+    auto&& thr = std::get<5>(input);
+    auto&& rsq = std::get<6>(input);
+    auto&& strong_beta = std::get<7>(input);
+    auto&& strong_grad = std::get<8>(input);
+    auto orig_strong_grad = strong_grad;
+    auto&& active_set = std::get<9>(input);
+    auto&& active_order = std::get<10>(input);
+    auto&& active_set_ordered = std::get<11>(input);
+    auto&& is_active = std::get<12>(input);
+    auto&& betas = std::get<13>(input);
+    auto&& rsqs = std::get<14>(input);
+    auto&& n_cds = std::get<15>(input);
+    auto&& n_lmdas = std::get<16>(input);
+
+    for (auto _ : state) {
+        state.PauseTiming();
+        reset(orig_strong_grad, strong_beta, strong_grad, active_set,
+              active_order, active_set_ordered, is_active, n_cds, n_lmdas, rsq);
+        state.ResumeTiming();
+        lasso(A, s, strong_set, strong_order, 
+              strong_A_diag, lmdas, max_cds, thr, rsq, strong_beta, 
+              strong_grad, active_set, active_order, active_set_ordered,
+              is_active, betas, rsqs, 
+              n_cds, n_lmdas);
     }
 
-    std::vector<uint32_t> strong_set(p);
-    std::iota(strong_set.begin(), strong_set.end(), 0);
+    state.counters["n_cds"] = n_cds;
+    state.counters["n_active_max"] = active_set.size();
+}
 
-    auto strong_order = strong_set;
+BENCHMARK_REGISTER_F(LassoFixture, dense)
+    -> Arg(10)
+    -> Arg(50)
+    -> Arg(100)
+    -> Arg(500)
+    -> Arg(1000)
+    -> Arg(2000);
 
-    const Eigen::VectorXd strong_A_diag = A.diagonal();
+// ==========================================
+// BENCHMARK BlockMatrix<Dense>
+// ==========================================
 
-    std::vector<double> strong_beta(strong_set.size(), 0);
-    std::vector<double> strong_grad(strong_set.size());
-    for (size_t i = 0; i < strong_grad.size(); ++i) {
-        strong_grad[i] = r[strong_set[i]];
+struct LassoBlockFixture
+    : LassoFixture,
+      tools::BlockMatrixUtil
+{
+    using butil = tools::BlockMatrixUtil;
+    using bmat_t = BlockMatrix<Eigen::MatrixXd>;
+
+    static auto generate_data(size_t n, size_t p, size_t L, size_t seed)
+    {
+        std::vector<Eigen::MatrixXd> mat_list(L);
+        Eigen::VectorXd r(L * p);
+        for (size_t l = 0; l < L; ++l) {
+            auto&& out = LassoFixture::generate_data(n, p, seed);
+            mat_list[l] = std::move(std::get<0>(out));
+            r.segment(l*p, p) = std::get<1>(out);
+        }
+        return std::make_tuple(mat_list, r);
     }
-    std::vector<uint32_t> active_set;
-    std::vector<uint32_t> active_order;
-    std::vector<uint32_t> active_set_ordered;
-    std::vector<bool> is_active(strong_set.size(), false);
+};
 
-    const auto orig_strong_grad = strong_grad;
+BENCHMARK_DEFINE_F(LassoBlockFixture, block_dense)(benchmark::State& state) 
+{
+    size_t L = state.range(0);
+    size_t p = state.range(1);
+    size_t n = 100;
+    size_t seed = 30;
+    double s = 0.5;
 
-    std::vector<Eigen::SparseVector<double>> betas(lmdas.size());
-    std::vector<double> rsqs(lmdas.size());
+    auto&& data = generate_data(n, p, L, seed);
+    auto&& mat_list = std::get<0>(data);
+    bmat_t A(mat_list);
+    auto&& r = std::get<1>(data);
 
-    size_t n_cds = 0;
-    size_t n_lmdas = 0;
-    double rsq = 0;
+    auto&& input = make_input(A, r);
+    auto&& strong_set = std::get<0>(input);
+    auto&& strong_order = std::get<1>(input);
+    auto&& strong_A_diag = std::get<2>(input);
+    auto&& lmdas = std::get<3>(input);
+    auto&& max_cds = std::get<4>(input);
+    auto&& thr = std::get<5>(input);
+    auto&& rsq = std::get<6>(input);
+    auto&& strong_beta = std::get<7>(input);
+    auto&& strong_grad = std::get<8>(input);
+    auto orig_strong_grad = strong_grad;
+    auto&& active_set = std::get<9>(input);
+    auto&& active_order = std::get<10>(input);
+    auto&& active_set_ordered = std::get<11>(input);
+    auto&& is_active = std::get<12>(input);
+    auto&& betas = std::get<13>(input);
+    auto&& rsqs = std::get<14>(input);
+    auto&& n_cds = std::get<15>(input);
+    auto&& n_lmdas = std::get<16>(input);
         
     for (auto _ : state) {
         state.PauseTiming();
@@ -120,13 +239,16 @@ BENCHMARK_DEFINE_F(LassoFixture, lasso_bench)(benchmark::State& state)
     state.counters["n_active_max"] = active_set.size();
 }
 
-BENCHMARK_REGISTER_F(LassoFixture, lasso_bench)
-    -> Arg(10)
-    -> Arg(50)
-    -> Arg(100)
-    -> Arg(500)
-    -> Arg(1000)
-    -> Arg(2000);
+BENCHMARK_REGISTER_F(LassoBlockFixture, block_dense)
+    -> Args({2, 10})
+    -> Args({2, 50})
+    -> Args({2, 100})
+    -> Args({2, 500})
 
+    -> Args({2,  100})
+    -> Args({16, 100})
+    -> Args({32, 100})
+    -> Args({64, 100})
+    ;
 }
 } // namespace ghostbasil
